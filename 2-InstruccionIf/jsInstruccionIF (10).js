341658dc1f9cb23989f4cf29f897a8f11ecbb69c
FrancoUTN <52534851+FrancoUTN@users.noreply.github.com>
2019-07-12 12:13:15 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
+	//Genero el número RANDOM entre 1 y 10
+  var nota;
+
+  nota = Math.floor(Math.random()*(10))+1;
+
+  if(nota >= 9){
+    alert("Excelente.");
+  } else if(nota > 4){
+    alert("Aprobó.")
+  } else{
+    alert("Vamos, la próxima se puede.")
+  }
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -8,9 +8,10 @@ function mostrar()
 
   if(edad >= 18){
     alert("Usted es mayor de edad.")
-    }   else if(edad >= 13){
+    }   else {if(edad >= 13){
      alert("Usted es adolescente.")
     }     else{
        alert("Usted es un/a niñ@")
      }
   }
+}
